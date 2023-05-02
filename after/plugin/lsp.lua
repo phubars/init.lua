02ac84e95137a5b2c6015d7483f7460ae8dc7d5f
Phu Huynh <phubars@users.noreply.github.com>
2023-05-01 21:55:14 -0700

    spacing

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,9 @@
 local lsp = require('lsp-zero')
 
 lsp.preset("recommended")
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true })
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true }
+)
+
 lsp.setup()
 
