d43b6eee46b427cc1a218911f495598ebaca076b
Phu Huynh <phubars@users.noreply.github.com>
2023-05-07 14:45:02 -0700

    re-anble ale

--- after/plugin/ale.lua
+++ after/plugin/ale.lua
@@ -1,8 +1,8 @@
 vim.g.ale_fix_on_save = 1
 vim.g.ale_fixers = {
---    '*' = { 'remove_trailing_lines', 'trim_whitespace' },
+    ['*'] = { 'remove_trailing_lines', 'trim_whitespace' },
     typescript = { "prettier", "eslint" },
     javascript = { "prettier", "eslint" },
     terraform = { 'terraform' },
+    lua = { 'lua-format' }
 }
-
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,5 +5,15 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true }
 )
 
-lsp.setup()
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+end)
 
+-- lsp.format_on_save({
+--     servers = {
+--         ['lua_ls'] = { 'lua' },
+--         ['eslint'] = { 'typescript', 'javascript' },
+--     }
+-- })
+
+lsp.setup()
