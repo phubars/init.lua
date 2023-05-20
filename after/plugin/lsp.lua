11a520129c3b7812630ba5890e0737bed3a5ba46
Phu Huynh <phubars@users.noreply.github.com>
2023-05-20 12:49:36 -0700

    Swicch to 2.x for lsp-zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,11 +9,4 @@ lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({ buffer = bufnr })
 end)
 
--- lsp.format_on_save({
---     servers = {
---         ['lua_ls'] = { 'lua' },
---         ['eslint'] = { 'typescript', 'javascript' },
---     }
--- })
-
 lsp.setup()
