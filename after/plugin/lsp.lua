d0abaf3039ba2838129a3dededb9d387ab9d78a8
Phu Huynh <phubars@users.noreply.github.com>
2023-05-23 22:44:01 -0700

    try to add mapping

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,3 +10,20 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 lsp.setup()
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }
+})
