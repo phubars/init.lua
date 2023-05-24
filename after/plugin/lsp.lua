44caf3d3d217c7f2653a3a91501f62c002755086
Phu Huynh <phubars@users.noreply.github.com>
2023-05-23 22:52:42 -0700

    simplify dependencies

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,7 +12,6 @@ end)
 lsp.setup()
 
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
   mapping = {
@@ -21,9 +20,5 @@ cmp.setup({
 
     -- Ctrl+Space to trigger completion menu
     ['<C-Space>'] = cmp.mapping.complete(),
-
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
   }
 })
