2b0047a2aecd643ec97f85ad659f8b7ceb085e3b
Phu Huynh <phubars@users.noreply.github.com>
2024-02-05 16:40:52 -0800

    migrate lsp-zero to v3.x

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,19 +1,28 @@
-local lsp = require('lsp-zero')
+local lsp_zero = require('lsp-zero')
 
-lsp.preset("recommended")
+lsp_zero.on_attach(function(client, bufnr)
+    lsp_zero.default_keymaps({ buffer = bufnr })
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = { 'tsserver' },
+    handlers = {
+        lsp_zero.default_setup,
+    }
+})
+
+-- lsp.preset("recommended")
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true }
 )
 
-lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
-end)
-
-lsp.setup()
-
 local cmp = require('cmp')
+local cmp_format = lsp_zero.cmp_format()
 
 cmp.setup({
+  formatting = cmp_format,
   mapping = {
     -- `Enter` key to confirm completion
     ['<CR>'] = cmp.mapping.confirm({select = false}),
