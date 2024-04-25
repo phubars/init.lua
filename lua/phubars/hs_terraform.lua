ddce3165467b5d5be48b1e2bc3e25a36baf1479b
Phu Huynh <phubars@users.noreply.github.com>
2024-04-25 08:44:03 -0700

    add sw command

--- lua/phubars/hs_terraform.lua
+++ lua/phubars/hs_terraform.lua
@@ -6,7 +6,8 @@ vim.keymap.set("n", "<leader>tr", ":terminal rm -rf .terraform<CR>")
 vim.keymap.set("n", "<leader>tp", ":terminal make plan<CR>")
 vim.keymap.set("n", "<leader>tpd", ":terminal make plan-dev<CR>")
 vim.keymap.set("n", "<leader>tpi", ":terminal make plan-integration<CR>")
-vim.keymap.set("n", "<leader>tps", ":terminal make plan-staging_usw2<CR>")
+vim.keymap.set("n", "<leader>tpsw", ":terminal make plan-staging_usw2<CR>")
+vim.keymap.set("n", "<leader>tps", ":terminal make plan-staging<CR>")
 vim.keymap.set("n", "<leader>tpp", ":terminal make plan-prod<CR>")
 
 -- state
