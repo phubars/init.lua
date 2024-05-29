10a6553e1fa43a8041546551b01b92fed993739e
Phu Huynh <phubars@users.noreply.github.com>
2024-05-29 11:17:29 -0700

    disable copilot

--- lua/phubars/packer.lua
+++ lua/phubars/packer.lua
@@ -71,8 +71,5 @@ return require('packer').startup(function(use)
       "iamcco/markdown-preview.nvim",
       run = function() vim.fn["mkdp#util#install"]() end,
   })
-
-  -- copilot
-  use('github/copilot.vim')
 end)
 
