03112bcd07e6e9e898275e18eff24ee73b32fa0a
Phu Huynh <phubars-mercury@users.noreply.github.com>
2025-02-27 14:10:38 -0800

    update colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -9,3 +9,8 @@ end
 vim.cmd.colorscheme "catppuccin"
 vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+
+require("catppuccin").setup {
+    integrations = {
+    }
+}
